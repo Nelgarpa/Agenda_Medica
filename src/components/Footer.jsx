eb98968783cd90b8baae3823c660a9c8fb6258b6
Nelcy <nelgarpa22@gmail.com>
2025-05-14 12:35:54 +0200

    mejorar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 export const Footer = () => {
-    return (
-      <footer className="text-center text-muted py-4 border-top mt-5">
-        <small>Desarrollado con ❤️ por Nelcy García © {new Date().getFullYear()}</small>
-      </footer>
-    );
-  };
+  return (
+    <footer className="text-center text-muted py-4 border-top mt-5">
+      <small>
+        Desarrollado con ❤️ por Nelcy García © {new Date().getFullYear()} <br />
+        <a
+          href="https://www.linkedin.com/in/nelcy-garcia"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-decoration-none text-primary"
+        >
+          LinkedIn
+        </a>
+      </small>
+    </footer>
+  );
+};
